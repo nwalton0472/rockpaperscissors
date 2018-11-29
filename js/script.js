4818fc1f9742b56a8e6b1f830ec8f147e02d26bd
Neota Walton <neota1951@gmail.com>
2018-11-29 23:31:08 +0000

    great

--- js/script.js
+++ js/script.js
@@ -6,11 +6,29 @@
 /* global $ */
 
 // DOCUMENT READY FUNCTION BELOW
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var randomNumber = 0;
 
-$(".shoot").click(function(){
-    $("#userChoice").text($("#input").val());
+$("#shoot").click(function(){
+    userChoice = $("#input").val();
+    randomNumber = Math.random();
+    $("#userChoice").text(userChoice);
+    
+    console.log();
+    if (randomNumber < .333){
+    computerChoice = "rock"
+    $("#computerChoice").text(computerChoice);
+    }
+  else if (randomNumber < .666) {
+      userChoice = "paper"
+      $("#userChoice").text(userChoice);
+  } else {
+        computerChoice = "siccors"
+        $("#computerChoice").text(computerChoice);
+    }
 });
 
-$(".shoot").click(function(){
-    $("#computerChoice").text($("#input").val());
-});
+
+
